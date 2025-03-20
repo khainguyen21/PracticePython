fb2e88bc18733a2340259d1891db8471b0ec1e0f
Khai Nguyen <khainguyen2004@gmail.com>
2025-03-19 22:43:25 -0700

    New Version

--- exercise.py
+++ exercise.py
@@ -1,12 +1,11 @@
-### Lesson 1
+## Lesson 1
 
 # # Ex1: Write a program to count positive and negative numbers in a list
 # data1 = [-10, -21, -4, -45, -66, 93, 11, -4, -6, 12, 11, 4]
 #
 # # Initialize counters for positive and negative numbers
 # positive = 0
-# negative = 0
-
+#
 # # Loop through each number in a list
 # for number in data1:
 #
@@ -14,9 +13,7 @@
 #     if number > 0:
 #         positive += 1
 #
-#     # Otherwise, increment negative counter by one
-#     else:
-#         negative += 1
+# negative = len(data1) - positive
 #
 # # Display total positive and negative number in a list
 # print("Total positive number in a list is ", positive)
@@ -193,3 +190,42 @@ import numpy as np
 #     # word = item[0]
 #     # frequency = item[1]
 #     print(f"{word}: {frequency}")
+
+
+
+import gensim.downloader as api
+import numpy as np
+
+# 25, 50, 100 or 200. Số càng lớn thì càng chính xác, nhưng chạy càng lâu các bạn nhé
+model = api.load("glove-twitter-50")
+word = "beautiful"
+print(model[word])
+
+print("1----------")
+result = model.most_similar(word, topn=10)
+print(result)
+
+print("2----------")
+result = model.most_similar(positive=["january", "february"], topn=10)
+print(result)
+
+print("3----------")
+result = model.similarity("man", "woman")
+print(result)
+
+print("4----------")
+result = model.most_similar(positive=["woman", "king"], negative=["man"], topn=1)
+print(result)
+
+print("5----------")
+result = model.most_similar(positive=["berlin", "vietnam"], negative=["hanoi"], topn=1)
+print(result)
+
+print("6----------")
+result = model.similarity("marriage", "happiness")
+print(result)
+
+
+#TODO: Các bạn thử viết 2 cách khác nhau để tính cosine similarity
+# giữa 2 vector nhé. Kết quả giống với khi dùng model.similarity() là được
+# 1 cách là dùng numpy, 1 cách là không dùng numpy nhé
