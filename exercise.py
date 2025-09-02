e323c2e804b2803b7cb023203e58fba0a139c89b
Khai Nguyen <khainguyen2004@gmail.com>
2025-09-01 21:18:41 -0700

    practice python for coding interview

--- exercise.py
+++ exercise.py
@@ -840,3 +840,24 @@ def isHappy(n: int):
 
 
 print(isHappy(19))
+
+
+def search(nums, target) :
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+
+        m = (l + r) // 2
+
+        if nums[m] < target:
+            l = m + 1
+
+        elif nums[m] > target:
+            r = m - 1
+
+        else:
+            return m
+
+    return -1
+
+print(search([-1,0,2,4,6,8], 4),)
